608ca0b6b3a96934bb0e7aa89ca5c42740a028cf
shtypuliak.jr <shtypuliak.jr@gmail.com>
2023-11-07 01:08:50 +0200

    CS - added modules for web-apps

---
+++ meta-service/src/main/java/edu/nau/cs/meta/service/MetaServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.nau.cs.meta.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaServiceApplication.class, args);
+	}
+
+}
