e2d7a17d8254e5e7f7e85f5c38ac1f6abd41b0bc
shtypuliakJr <shtypuliak.jr@gmail.com>
2023-11-12 01:40:28 +0200

    CS - init adding api documentation for main endpoints

---
+++ file-service/src/main/java/edu/nau/cs/file/service/controller/UploadFileController.java
@@ -0,0 +1,10 @@
+package edu.nau.cs.file.service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UploadFileController implements FileUploadControllerApi {
+
+}
