90172790a45f9c6645e94cf6b48d73dfe34b352b
shtypuliakJr <shtypuliak.jr@gmail.com>
2023-11-15 19:05:13 +0200

    CS - added s3 configuration

---
+++ file-service/src/main/java/edu/nau/cs/file/service/config/AwsS3Config.java
@@ -0,0 +1,71 @@
+package edu.nau.cs.file.service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+import java.time.Duration;
+
+@Configuration
+public class AwsS3Config {
+
+    private final String awsRegion;
+    private final String awsS3FsEndpoint;
+
+    @Autowired
+    public AwsS3Config(@Value(value = "${aws.region}") String awsRegion,
+                       @Value(value = "${aws.s3.fs.endpoint}") String awsS3FsEndpoint) {
+        this.awsRegion = awsRegion;
+        this.awsS3FsEndpoint = awsS3FsEndpoint;
+    }
+
+    @Bean
+    public S3Client s3Client(AwsCredentialsProvider awsCredentialsProvider,
+                             ClientOverrideConfiguration clientOverrideConfiguration) {
+        return S3Client.builder()
+                .region(Region.of(awsRegion))
+                .endpointOverride(URI.create(awsS3FsEndpoint))
+                .credentialsProvider(awsCredentialsProvider)
+                .overrideConfiguration(clientOverrideConfiguration)
+                .httpClient(ApacheHttpClient.create())
+                .build();
+    }
+
+    @Bean
+    public S3AsyncClient s3AsyncClient(AwsCredentialsProvider awsCredentialsProvider,
+                                       ClientOverrideConfiguration clientOverrideConfiguration) {
+        return S3AsyncClient.builder()
+                .region(Region.of(awsRegion))
+                .endpointOverride(URI.create(awsS3FsEndpoint))
+                .credentialsProvider(awsCredentialsProvider)
+                .overrideConfiguration(clientOverrideConfiguration)
+                .build();
+    }
+
+    @Bean
+    @Primary
+    protected ClientOverrideConfiguration clientOverrideConfiguration() {
+        return ClientOverrideConfiguration.builder()
+                .apiCallAttemptTimeout(Duration.ofSeconds(1))
+                .retryPolicy(RetryPolicy.builder().numRetries(10).build())
+                .build();
+    }
+
+    @Bean
+    @Primary
+    protected AwsCredentialsProvider awsCredentialsProvider() {
+        return DefaultCredentialsProvider.create();
+    }
+
+}
--- file-service/src/test/java/edu/nau/cs/file/service/FileServiceApplicationTest.java
+++ file-service/src/test/java/edu/nau/cs/file/service/FileServiceApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileServiceApplicationTests {
+class FileServiceApplicationTest {
 
 	@Test
 	void contextLoads() {
